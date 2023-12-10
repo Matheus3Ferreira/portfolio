feecf747a589ae1ceb898490dec6c653d12f1ecf
Matheus Ferreira <ferreiromagrelo@gmail.com>
2023-12-10 16:24:34 -0300

    feat: maintence

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import Header from './components/Header';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import "./style/app.scss"
+import React from "react";
+// import Header from "./components/Header";
+// import Contact from "./pages/Contact";
+// import Home from "./pages/Home";
+// import Portfolio from "./pages/Portfolio";
+import "./style/styles.css";
+import Maintence from "./pages/maintence/Maintence";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Portfolio />
-      <Contact />
-    </div>
+    // <div className="App">
+    //   <Header />
+    //   <Home />
+    //   <Portfolio />
+    //   <Contact />
+    // </div>
+    <Maintence />
   );
 }
 
